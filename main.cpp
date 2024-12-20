f615752ddde874c687addf8a18c7097b6938f92a
caioescorcio <caioeldourado@gmail.com>
2024-12-20 15:06:30 -0300

    💾feat/Main Primeiro Passo

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <thread>
+#include <cstdio>
+#include <Windows.h>
+#include <jni.h>
+
+void MainThread(HMODULE instance) {
+
+	JavaVM* p_jvm{ nullptr };
+	jint result = JNI_GetCreatedJavaVMs(&p_jvm, 1, nullptr);
+
+	void* p_env{ nullptr };
+	p_jvm->AttachCurrentThread(&p_env, nullptr);
+
+	while (!GetAsyncKeyState(VK_END)) {
+		printf("%d", GetAsyncKeyState(VK_DOWN));
+	}
+	FreeLibrary(instance);
+}
+
+bool __stdcall DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+
+	static FILE* p_file{ nullptr };
+	static std::thread main_thread;
+
+	if (reason == DLL_PROCESS_ATTACH) {
+
+		AllocConsole();
+		freopen_s(&p_file, "CONOUT$", "w", stdout);
+		main_thread = std::thread([instance] { MainThread(instance);});
+		if (main_thread.joinable()) {
+			main_thread.detach();
+		}
+	}
+	else if (reason == DLL_PROCESS_DETACH) {
+
+		fclose(p_file);
+		FreeConsole();
+
+	}
+
+	return true;
+}
